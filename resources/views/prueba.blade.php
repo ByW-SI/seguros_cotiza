6cb45806a2bd747f7b2e49e5551acb8f6dfdf310
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-01-13 13:51:08 -0600

    Update prueba.blade.php

--- resources/views/prueba.blade.php
+++ resources/views/prueba.blade.php
@@ -4,8 +4,8 @@
 	<input type="submit" name="" value="Enviar">
 </form>
 
-<div class="tab-pane fade" id="paso2" role="tabpanel" aria-labelledby="paso2-tab">
-	<polizas v-bind:cliente="cliente" :img="img" :alert="alert" :getcotizacion="getcotizacion" :cotizacion="cotizacion" @emitirgs="cotizacion=$event" @emitirqua="cotizacion=$event" @emitirana="cotizacion=$event" @emitirgnp="cotizacion=$event">
+<div class="tab-pane fade" id="paso3" role="tabpanel" aria-labelledby="paso3-tab">
+	<formulario :cotizacion="cotizacion" :img="img" :cliente="cliente" :alert="alert"></formulario>
 </div>
 
 @endsection
