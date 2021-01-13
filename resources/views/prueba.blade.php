5e8e9090e10116af559762bf7ab4ed2aece52160
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-01-13 13:55:27 -0600

    act

--- resources/views/prueba.blade.php
+++ resources/views/prueba.blade.php
@@ -4,8 +4,8 @@
 	<input type="submit" name="" value="Enviar">
 </form>
 
-<div class="tab-pane fade" id="paso3" role="tabpanel" aria-labelledby="paso3-tab">
-	<formulario :cotizacion="cotizacion" :img="img" :cliente="cliente" :alert="alert"></formulario>
+<div >
+	<example-component></example-component>
 </div>
 
 @endsection
