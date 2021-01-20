a38d45e0dad15a3014c7924ab3dc37318fc6b41a
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-01-20 15:41:29 -0600

    prueba

--- resources/views/prueba.blade.php
+++ resources/views/prueba.blade.php
@@ -6,7 +6,16 @@
 
 <div >
 	<h2>Hola estas son la spruebas</h2>
-	<example> </example>
+	<example-component></example-component>
+<example-component></example-component>
 </div>
+<div class="tab-pane fade show active" id="paso1" role="tabpanel" aria-labelledby="paso1-tab">
+	<cotizacion v-bind:cliente="cliente" :img="img" :getcotizacion="getcotizacion" :alert="alert"></cotizacion>
+</div>
+<div class="tab-pane fade" id="paso2" role="tabpanel" aria-labelledby="paso2-tab">
+	<polizas v-bind:cliente="cliente" :img="img" :alert="alert" :getcotizacion="getcotizacion" :cotizacion="cotizacion" @emitirgs="cotizacion=$event" @emitirqua="cotizacion=$event" @emitirana="cotizacion=$event" @emitirgnp="cotizacion=$event">
+</div>
+<div class="tab-pane fade" id="paso3" role="tabpanel" aria-labelledby="paso3-tab">
+	<formulario :cotizacion="cotizacion" :img="img" :cliente="cliente" :alert="alert"></formulario>
 
 @endsection
