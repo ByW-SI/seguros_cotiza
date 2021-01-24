94ff9dcfb7fb35c0b399226aa58652915b75caab
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-01-24 16:50:32 -0600

    Update GNPController.php

--- app/Http/Controllers/GNPController.php
+++ app/Http/Controllers/GNPController.php
@@ -91,7 +91,7 @@ class GNPController extends Controller
 		 $carroceria = $this->getCarroceria($armadora, $submarca);
 		/*  dd($armadora,$carroceria); */
 		 $modelos    = $this->getModelos($modelo, $armadora, $carroceria);
-		/*  dd($modelos); */
+		 dd($modelos);
  		return response()->json(['modelosGNP'=>$modelos],201);
  	}
 
