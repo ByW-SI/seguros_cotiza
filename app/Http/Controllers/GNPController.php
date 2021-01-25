17604138b80551ffd6f5b8715096c6f37e3bf1ca
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-01-24 22:24:47 -0600

    Update GNPController.php

--- app/Http/Controllers/GNPController.php
+++ app/Http/Controllers/GNPController.php
@@ -588,7 +588,7 @@ class GNPController extends Controller
  				$primaTotal = $array_data["PAQUETES"]["PAQUETE"]["TOTALES"]["TOTAL_PRIMA"][2]["CONCEPTO_ECONOMICO"][10]["MONTO"];
  				break;
  		}
-/* 		 dd($array_data); */
+		/*  dd($array_data); */
  		return  "<EMISION>
 				  <SOLICITUD>
 				    <USUARIO>$this->user</USUARIO>
@@ -723,7 +723,7 @@ class GNPController extends Controller
 			$this->curl->post("https://api.service.gnp.com.mx/autos/wsp/emisor/emisor/emitir", $data);
 	        //convert the XML result into array
 			$array_data = json_decode(json_encode(simplexml_load_string($this->curl->response)), true);
-			/* dd($array_data); */
+			dd($array_data);
 	        return view('gnp.poliza',['response'=>$array_data]);
 	        // return response()->json(['cotizacionGNP'=>$array_data],201);
 		} catch (Exception $e) {
