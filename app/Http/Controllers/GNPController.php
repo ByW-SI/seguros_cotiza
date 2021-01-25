f406ee7977cbd8dbf629a11dcf562e38feef5cd2
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-01-24 19:14:25 -0600

    Update GNPController.php

--- app/Http/Controllers/GNPController.php
+++ app/Http/Controllers/GNPController.php
@@ -721,7 +721,8 @@ class GNPController extends Controller
 			// dd($data);
 			$this->curl->post("https://api.service.gnp.com.mx/autos/wsp/emisor/emisor/emitir", $data);
 	        //convert the XML result into array
-	        $array_data = json_decode(json_encode(simplexml_load_string($this->curl->response)), true);
+			$array_data = json_decode(json_encode(simplexml_load_string($this->curl->response)), true);
+			dd($array_data);
 	        return view('gnp.poliza',['response'=>$array_data]);
 	        // return response()->json(['cotizacionGNP'=>$array_data],201);
 		} catch (Exception $e) {
