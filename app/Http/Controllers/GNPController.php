a87cad47939298aa73862ae9e7e808e847a74574
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-01-24 20:39:36 -0600

    Update GNPController.php

--- app/Http/Controllers/GNPController.php
+++ app/Http/Controllers/GNPController.php
@@ -588,7 +588,7 @@ class GNPController extends Controller
  				$primaTotal = $array_data["PAQUETES"]["PAQUETE"]["TOTALES"]["TOTAL_PRIMA"][2]["CONCEPTO_ECONOMICO"][9]["MONTO"];
  				break;
  		}
-		 dd($primaTotal);
+		 dd($primaTotal,$primaNeta,$importeIVA);
  		return  "<EMISION>
 				  <SOLICITUD>
 				    <USUARIO>$this->user</USUARIO>
