f2c7e8ef84fa65012f6a54b1b5320eee8bd449bd
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-01-24 23:11:31 -0600

    Update poliza.blade.php

--- resources/views/gnp/poliza.blade.php
+++ resources/views/gnp/poliza.blade.php
@@ -28,7 +28,7 @@
 					<div class="col-6">
 						<h4>Su poliza se guardo con la siguiente información:</h4>
 					</div>
-					{{dd($array_data)}}
+					{{var_dump($array_data)}}
 					<div class="col-6">
 						<label class="control-label">Número de poliza:</label>
 						@if( isset($response['SOLICITUD']) )
